1f7d82360968e08fa9a81172055f7316cfce7998
spocasangre <00368718@uca.edu.sv>
2023-06-16 19:37:11 -0600

    adding advisory and type class

---
+++ restapi/src/main/java/com/example/restapi/models/entities/Advisory.java
@@ -0,0 +1,66 @@
+package com.example.restapi.models.entities;
+
+import javax.persistence.*;
+
+@Entity(name = "asesor")
+public class Advisory {
+
+    @Id
+    @Column(name = "id_asesor")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_asesor;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "telefono")
+    private Integer telefono;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "id_tipo_asesor")
+    private AdvisorType advisorType;
+
+    public Advisory(Long id_asesor, String nombre, Integer telefono, AdvisorType advisorType) {
+        super();
+        this.id_asesor = id_asesor;
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.advisorType = advisorType;
+    }
+
+    public Advisory() {
+        super();
+    }
+
+    public AdvisorType getAdvisorType() {
+        return advisorType;
+    }
+
+    public void setAdvisorType(AdvisorType advisorType) {
+        this.advisorType = advisorType;
+    }
+
+    public Long getId_asesor() {
+        return id_asesor;
+    }
+
+    public void setId_asesor(Long id_asesor) {
+        this.id_asesor = id_asesor;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(Integer telefono) {
+        this.telefono = telefono;
+    }
+}
