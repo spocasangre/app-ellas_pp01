b1e0918b8b89ac1f00c772ceeb04894e68ee10f1
spocasangre <00368718@uca.edu.sv>
2023-06-19 14:28:35 -0600

    adding freemaker conf class

---
+++ restapi/src/main/java/com/example/restapi/utils/FreemarkerConfig.java
@@ -0,0 +1,15 @@
+package com.example.restapi.utils;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.ui.freemarker.FreeMarkerConfigurationFactoryBean;
+
+@Configuration
+public class FreemarkerConfig {
+    @Bean
+    public FreeMarkerConfigurationFactoryBean getFreeMarkerConfiguration() {
+        FreeMarkerConfigurationFactoryBean fmConfigFactoryBean = new FreeMarkerConfigurationFactoryBean();
+        fmConfigFactoryBean.setTemplateLoaderPath("classpath:/templates/");
+        return fmConfigFactoryBean;
+    }
+}
