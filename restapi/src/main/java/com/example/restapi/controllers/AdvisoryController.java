57f0333b04310a0b2e69e156a414b836246b74a4
spocasangre <00368718@uca.edu.sv>
2023-06-19 18:04:40 -0600

    adding advisory controller

---
+++ restapi/src/main/java/com/example/restapi/controllers/AdvisoryController.java
@@ -0,0 +1,81 @@
+package com.example.restapi.controllers;
+
+import com.example.restapi.models.dtos.CreateAdvisoryDTO;
+import com.example.restapi.models.dtos.MessageResponse;
+import com.example.restapi.models.entities.AdvisorType;
+import com.example.restapi.models.entities.Advisory;
+import com.example.restapi.services.AdvisoryService;
+import com.example.restapi.services.AdvisoryTypeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/advisory")
+public class AdvisoryController {
+
+    @Autowired
+    AdvisoryService advisoryService;
+
+    @Autowired
+    AdvisoryTypeService advisoryTypeService;
+
+    @GetMapping("/{id}")
+    @PreAuthorize("hasRole('admin') or hasRole('usuario')")
+    public ResponseEntity<?> getByCategory(@PathVariable("id") Long id) {
+        try {
+            AdvisorType advisorType = advisoryTypeService.findById(id);
+            List<Advisory> advisoryList = advisoryService.getAdvisoriesByAdvisorType(advisorType);
+            if (advisoryList.isEmpty()) {
+                return ResponseEntity.ok(new MessageResponse(true, 1, advisoryList, "Lista vacia"));
+            } else {
+                return ResponseEntity.ok(new MessageResponse(true, 1, advisoryList, "Lista obtenida correctamente"));
+            }
+        } catch (Exception e) {
+            return ResponseEntity.ok(new MessageResponse(false, 7, null, "No se pudo obtener la lista de asesores"));
+        }
+    }
+
+    @PostMapping("")
+    @PreAuthorize("hasRole('admin')")
+    public ResponseEntity<?> createAdvisory(@RequestBody CreateAdvisoryDTO createAdvisoryDTO) {
+        try {
+            Advisory advisory = new Advisory();
+            AdvisorType advisorType = advisoryTypeService.findById(createAdvisoryDTO.getType());
+            advisory.setAdvisorType(advisorType);
+            advisory.setNombre(createAdvisoryDTO.getFullname());
+            advisory.setTelefono(createAdvisoryDTO.getNumber());
+            advisoryService.save(advisory);
+            return ResponseEntity.ok(new MessageResponse(true, 1, advisory, "Asesor creado!"));
+        } catch (Exception e) {
+            return ResponseEntity.ok(new MessageResponse(false, 7, null, e.getMessage()));
+        }
+    }
+
+    @GetMapping("/detail/{id}")
+    @PreAuthorize("hasRole('admin') or hasRole('usuario')")
+    public ResponseEntity<?> getAdvisoryDetail(@PathVariable("id") Long id) {
+        try {
+            Advisory advisory = advisoryService.findById(id);
+            return ResponseEntity.ok(new MessageResponse(true, 1, advisory, "Asesor obtenido!"));
+        } catch (Exception e) {
+            return ResponseEntity.ok(new MessageResponse(false, 7, null, "Nose pudo obtener el detalle del asesor"));
+        }
+    }
+
+    @DeleteMapping("/delete_advisor/{id}")
+    @PreAuthorize("hasRole('admin')")
+    public ResponseEntity<?> deleteAdvisory(@PathVariable("id") Long id) {
+        try {
+            Advisory advisory = advisoryService.findById(id);
+            advisoryService.delete(advisory);
+            return ResponseEntity.ok(new MessageResponse(true, 1, null, "Asesor eliminado"));
+        } catch (Exception e) {
+            return ResponseEntity.ok(new MessageResponse(false, 7, null, "No se pudo eliminar el asesor"));
+        }
+    }
+
+}
